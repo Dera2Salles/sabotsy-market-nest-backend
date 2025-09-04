4a6261e03383ad277e5682cbb990d00c16056ef7
Dera2S <dera.ah.14@gmail.com>
2025-09-04 08:52:58 +0300

    fix : handling exisiting product on insertion and adding get All

--- src/application/repository/ProductPrismaRepository.ts
+++ src/application/repository/ProductPrismaRepository.ts
@@ -11,18 +11,79 @@ export class ProductPrismaRepository implements ProductRepository {
 
   async insertMany(product: ProductEntity[]): Promise<RESULT<void, Error>> {
     try {
-      await this.prisma.product.createMany({
-        data: product.map((item) => ({
-          name: item.name,
-          category: item.category,
-          description: item.description,
-          price: item.price,
-          producerId: item.producerId,
-          unit: item.unit,
-        })),
+      const productExisting = await this.prisma.product.findMany({
+        where: {
+          OR: product.map((item) => ({
+            producerId: item.producerId,
+            name: item.name.toUpperCase(),
+          })),
+        },
+        select: { producerId: true, name: true, id: true, unit: true },
       });
 
+      const productToInsert = product.filter(
+        (product) =>
+          !productExisting.some(
+            (existing) => existing.name === product.name.toUpperCase(),
+          ),
+      );
+
+      if (productExisting.length > 0) {
+        await Promise.all(
+          productExisting.map(async (existingProduct) => {
+            const matchProduct = product.find(
+              (item) => item.name.toUpperCase() === existingProduct.name,
+            );
+
+            if (matchProduct) {
+              await this.prisma.product.update({
+                where: { id: existingProduct.id },
+                data: {
+                  unit: {
+                    increment: matchProduct.unit,
+                  },
+                },
+              });
+            }
+          }),
+        );
+      }
+      if (productToInsert.length > 0) {
+        await this.prisma.product.createMany({
+          data: productToInsert.map((item) => ({
+            name: item.name.toUpperCase(),
+            category: item.category,
+            description: item.description,
+            price: item.price,
+            producerId: item.producerId,
+            unit: item.unit,
+            filename: item.filename,
+          })),
+        });
+      }
+
       return success(undefined);
+    } catch (error) {
+      console.error(error);
+      return failure(Error());
+    }
+  }
+
+  async getAll(): Promise<RESULT<ProductEntity[], Error>> {
+    try {
+      const result = await this.prisma.product.findMany({});
+      const products: ProductEntity[] = result.map((item) => ({
+        id: item.id,
+        category: item.category,
+        description: item.description,
+        filename: item.filename as string,
+        name: item.name,
+        price: item.price,
+        producerId: item.producerId,
+        unit: item.unit,
+      }));
+
+      return success(products);
     } catch (error) {
       console.error(error);
       return failure(Error());
