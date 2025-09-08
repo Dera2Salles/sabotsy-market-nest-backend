0f9cabcbe441817c50df2b5d9f37b1b2c8f6c5f3
Dera2S <dera.ah.14@gmail.com>
2025-09-08 07:43:17 +0300

    fix : adding get data method

--- src/domain/repository/UserRepository.ts
+++ src/domain/repository/UserRepository.ts
@@ -4,13 +4,25 @@ import {
   UserAlreadyExistException,
   UserNotFoundException,
 } from '../Exceptions';
-import { loginReturnType } from '@/application/repository/UserPrismaRepository';
+import { LoginDto } from '@/auth/loginDto';
+import { ProductEntity } from '../Entities/Product';
+
+export interface UserData {
+  name: string;
+  product: ProductEntity[];
+}
 
 export abstract class UserRepository {
   abstract signIn(
     login: string,
-  ): Promise<Result<loginReturnType, UserNotFoundException>>;
+  ): Promise<Result<LoginDto, UserNotFoundException>>;
   abstract register(
     user: UserEntity,
   ): Promise<Result<void, UserAlreadyExistException>>;
+
+  abstract getData(
+    userId: string,
+    page: number,
+    limit: number,
+  ): Promise<Result<UserData>>;
 }
