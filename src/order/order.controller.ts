457f8cad6e76696abda8281cda0a01d8cbb9e7a3
Dera2S <dera.ah.14@gmail.com>
2025-09-08 07:44:27 +0300

    feat : adding http ok status response

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderEntity } from '@/domain/Entities/Order';
 
@@ -6,6 +6,7 @@ import { OrderEntity } from '@/domain/Entities/Order';
 export class OrderController {
   constructor(private service: OrderService) {}
 
+  @HttpCode(HttpStatus.OK)
   @Post()
   async addOrder(@Body() order: OrderEntity) {
     return this.service.callPlaceOrder(order);
